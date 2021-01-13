b3296b428c704adcafec52f4f5832710b38f702e
Julien Dehos <dehos@univ-littoral.fr>
2021-01-13 19:16:50 +0100

    nix gym server

--- shell-gym.nix
+++ shell-gym.nix
@@ -1,10 +1,22 @@
-# https://github.com/openai/gym-http-api
-# nix-shell --run "python gym_http_server.py"
+# nix-shell shell-gym.nix 
+# gym_http_server
 
 with import <nixpkgs> {};
 
-(pkgs.python3.withPackages(ps: [ 
-  ps.flask
-  ps.gym
-])).env
+let
+  gym-http-api-src = fetchTarball {
+    url = "https://github.com/openai/gym-http-api/archive/15b68c3.tar.gz";
+    sha256 = "1zl5s79xy14ghpijy5s7pxjlfgng5hnyh0zbfn24hwhkk8161hwy";
+  };
+
+  mypython = pkgs.python3.withPackages(ps: [ 
+    ps.flask
+    ps.gym
+  ]);
+
+in mkShell {
+  shellHooks = ''
+    alias gym_http_server="${mypython}/bin/python ${gym-http-api-src}/gym_http_server.py"
+  '';
+}
 
